7129e9aa6ad3d963d324b9bac288806b4fe33161
ivy <597055914@qq.com>
2014-06-21 09:56:37 +0800

    sync合一算法

---
+++ Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Codebox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TestBox = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ResultBox = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Codebox
+        '
+        Me.Codebox.Location = New System.Drawing.Point(12, 12)
+        Me.Codebox.Multiline = True
+        Me.Codebox.Name = "Codebox"
+        Me.Codebox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.Codebox.Size = New System.Drawing.Size(319, 240)
+        Me.Codebox.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(270, 258)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 39)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Compel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TestBox
+        '
+        Me.TestBox.Location = New System.Drawing.Point(338, 12)
+        Me.TestBox.Multiline = True
+        Me.TestBox.Name = "TestBox"
+        Me.TestBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TestBox.Size = New System.Drawing.Size(224, 369)
+        Me.TestBox.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 308)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(316, 73)
+        Me.TextBox1.TabIndex = 3
+        '
+        'ResultBox
+        '
+        Me.ResultBox.Location = New System.Drawing.Point(18, 387)
+        Me.ResultBox.Multiline = True
+        Me.ResultBox.Name = "ResultBox"
+        Me.ResultBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.ResultBox.Size = New System.Drawing.Size(544, 83)
+        Me.ResultBox.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(618, 214)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(82, 28)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(572, 12)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(162, 196)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(572, 248)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox3.Size = New System.Drawing.Size(162, 222)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(746, 473)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ResultBox)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TestBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Codebox)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "NewProlog"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Codebox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TestBox As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ResultBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim ab As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Function GetEle(ByVal syn_dif As String) As String() 'act as structure
+        Dim a() As String
+        a = Split(syn_dif, ",")
+        Return a
+    End Function
+
+    Function syncretism(ByVal synf1 As String, synf2 As String) As String
+        Dim syn_fn() As String
+        If attr_IsSame(synf1, synf2) = True Then
+
+            syn_fn = GetEle(dif(synf1, synf2))
+            Return syn_fn(0) + "与" + syn_fn(1) + "有" + ab + "的关系"
+        Else
+            Return ""
+        End If
+    End Function
+
+    Function GetAttr(ByVal fn As String) As String '获得事件名称
+        Dim attr_len As Integer '事件长度
+        Dim attr_result As String = ""
+
+        attr_len = fn.LastIndexOf("(")
+        attr_result = fn.Remove(attr_len, fn.Length() - attr_len)
+
+        ab = attr_result
+
+        Return attr_result
+    End Function
+
+    Function dif(ByVal f1 As String, f2 As String) As String 'NULL失败,并且只针对不同的两个句子!@important
+        Dim i As Integer = 0
+        Dim j1 As Integer = 0
+        Dim j2 As Integer = 0
+        Dim t_f1 As String = ""
+        Dim t_f2 As String = ""
+
+        If f1 <> "" And f2 <> "" Then
+            While f1.ElementAt(i) = f2.ElementAt(i)
+                i += 1
+            End While
+
+            j1 = i
+            While j1 < f1.Length() - 1 And f1.ElementAt(j1) <> ","
+                ' p(a,y)  p(x,y)
+                j1 += 1
+                t_f1 = f1.Remove(j1, f1.Length() - j1)
+                t_f1 = t_f1.Replace(GetAttr(f1) + "(", "") '不同的部分
+            End While
+
+            j2 = i
+            While j2 < f2.Length() - 1 And f2.ElementAt(j2) <> ","
+                ' p(a,y)  p(x,y)
+                j2 += 1
+                t_f2 = f2.Remove(j2, f2.Length() - j2)
+                t_f2 = t_f2.Replace(GetAttr(f2) + "(", "") '不同的部分
+            End While
+
+            Return t_f1 + "," + t_f2
+        Else
+            Return ""
+        End If
+
+        Return ""
+    End Function
+
+    Function attr_IsSame(ByVal attr_f1 As String, attr_f2 As String) As Boolean '事件是否相同
+        Dim atf1 As String = ""
+        Dim atf2 As String = ""
+
+        atf1 = GetAttr(attr_f1)
+        atf2 = GetAttr(attr_f2)
+
+        If atf1 = atf2 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Debug.WriteLine(syncretism("p(a,y)", "p(x,y)"))
+        TextBox3.Text += syncretism("p(a,y)", "p(x,y)")
+    End Sub
+End Class
